29d712a52e76ab0cb8c7b5583cfec88a1caee077
anshika <jainanshika2412@gnail.com>
2025-02-22 20:51:50 +0530

    inital commit

---
+++ src/main/java/com/junit/easymock/app/JunitAndEasyMockApp/JunitAndEasyMockAppApplication.java
@@ -0,0 +1,13 @@
+package com.junit.easymock.app.JunitAndEasyMockApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitAndEasyMockAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitAndEasyMockAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junit/easymock/app/JunitAndEasyMockApp/JunitAndEasyMockAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junit.easymock.app.JunitAndEasyMockApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitAndEasyMockAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
